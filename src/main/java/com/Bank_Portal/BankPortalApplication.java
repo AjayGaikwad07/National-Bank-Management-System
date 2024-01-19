5bb17bfaa656f8ea69628f2381f107da0fa6d189
Ajeight <ajaygaikwad3131@gmail.com>
2024-01-19 13:11:51 +0530

    Initial commit

---
+++ src/main/java/com/Bank_Portal/BankPortalApplication.java
@@ -0,0 +1,14 @@
+package com.Bank_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankPortalApplication.class, args);
+		System.out.println("Hello Bank");
+	}
+
+}
